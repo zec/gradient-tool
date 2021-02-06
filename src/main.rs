89276eeed829409bdcf7ecc31de365726a7c3aaa
Zachary Catlin <z@zc.is>
2021-02-06 18:36:01 -0500

    Added non-crashy handling of errors in WebView creation
    ...though there's still room for improvement.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 extern crate web_view;
 
-use web_view::WebViewBuilder;
+use web_view::{WebViewBuilder,WVResult};
 use web_view::Content::*;
 
 fn main() {
     println!("Hello, world!");
 
-    let wv = WebViewBuilder::new()
+    match run_webview() {
+      Ok(_)  => println!("ran successfully"),
+      Err(e) => println!("Error: {}", e)
+    }
+}
+
+fn run_webview() -> WVResult<()> {
+    let mut wv = WebViewBuilder::new()
         .title("Hi there!")
         .content(Html("<html><head><title>.</title><body><p>Hello, world</p></body></html"))
-        .size(500, 900)
+        .size(600, 800)
         .user_data(())
         .invoke_handler(|_wv, _arg| Ok(()))
-        .build().unwrap();
+        .build()?;
 
-    wv.run().unwrap();
+    wv.run()?;
+
+    Ok(())
 }
