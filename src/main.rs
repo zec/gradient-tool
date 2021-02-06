6360fc2e01bc4d92119c3599bf8a7ef8909e241b
Zachary Catlin <z@zc.is>
2021-02-06 18:21:24 -0500

    Added minimal example of using webview

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+extern crate web_view;
+
+use web_view::WebViewBuilder;
+use web_view::Content::*;
+
 fn main() {
     println!("Hello, world!");
+
+    let wv = WebViewBuilder::new()
+        .title("Hi there!")
+        .content(Html("<html><head><title>.</title><body><p>Hello, world</p></body></html"))
+        .size(500, 900)
+        .user_data(())
+        .invoke_handler(|_wv, _arg| Ok(()))
+        .build().unwrap();
+
+    wv.run().unwrap();
 }
