d5f94cbd243a033cfad188cf17f725ed4652fb27
Zachary Catlin <z@zc.is>
2021-02-07 00:03:09 -0500

    Added indicators of licensing

--- build.rs
+++ build.rs
@@ -1,3 +1,20 @@
+/*
+   Copyright 2021 Zachary Catlin
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
 extern crate urlencoding;
 
 use std::{env,mem};
@@ -9,7 +26,7 @@ fn main() {
 
     let f_name = format!("{}/consts.rs", out_dir);
 
-    println!("cargo:rerun-if-changed=build/build.rs");
+    println!("cargo:rerun-if-changed={}/build.rs", src_dir);
     println!("cargo:rerun-if-changed={}", f_name);
 
     let mut f = BufWriter::new(File::create(f_name).unwrap());
