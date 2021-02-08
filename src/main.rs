009ac4520ab444d4e4b6f6e26620567b4c62ed25
Zachary Catlin <z@zc.is>
2021-02-08 16:05:40 -0500

    Switched to loading HTML after WebView construction
    The advantages of this new approach are that (1) we can include the file
    as-is without any compile- or run-time percent-encoding, so (2) we don't
    need a build script anymore. The executable should be slightly smaller, too.

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,6 @@ extern crate webbrowser;
 
 use web_view::WVResult;
 
-mod consts {
-    include!(concat!(env!("OUT_DIR"), "/consts.rs"));
-}
-
 fn main() {
     let result = run_webview();
 
@@ -33,18 +29,21 @@ fn main() {
     }
 }
 
+const UI_HTML: &'static str = include_str!("../ui.html");
+
 fn run_webview() -> WVResult<()> {
     use web_view::*;
 
-    let wv = WebViewBuilder::new()
+    let mut wv = WebViewBuilder::new()
         .title("Color space gradient tool")
-        .content(Content::Url(consts::UI_HTML))
+        .content(Content::Url("data:text/html,%20"))
         .size(600, 800)
         .min_size(600, 50)
         .user_data(())
         .invoke_handler(|_wv, arg| { drop(webbrowser::open(arg)); Ok(())})
         .build()?;
 
+    wv.set_html(UI_HTML)?;
     wv.run()?;
 
     Ok(())
